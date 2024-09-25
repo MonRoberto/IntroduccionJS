1de5c2940f4fdf3b945bf8e63ea1bd4ea2777ac1
MonRoberto <roberto.quiroz117@outlook.com>
2024-09-25 17:17:26 -0600

    Variables en JavaScript

---
+++ js/01.js
@@ -0,0 +1,24 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Iniciamos la variable pero sin valor
+
+//producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Inicializar multiples variables
+
+//var 1-disponible; No es posible poner numeros ni guiones medio al principio
+var disponible_; // Si es posible gui bajo
+
+//Estilos para las variables
+var nombre_producto = 'Monitor HD'; // Underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; // Sin nombre
+
+ console.log(producto);
