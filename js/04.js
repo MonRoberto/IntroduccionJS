ebf84aae5e6c6719eb6a0c292c6659837ce05bb9
MonRoberto <roberto.quiroz117@outlook.com>
2024-09-27 13:49:24 -0600

    Strings o Cadenas de Texto

---
+++ js/04.js
@@ -0,0 +1,9 @@
+// String o cadenas de texto
+
+const producto = "Monitor de 20\""; // o '"Monitor de 20"'
+//const producto2 = String('Monitor 30 Pulgadas'); No son tan comun
+//const producto3 = new String('Monitor 50 Pulgadas'); No son tan comun
+
+console.log(producto)
+//console.log(producto2)
+//console.log(producto3)
