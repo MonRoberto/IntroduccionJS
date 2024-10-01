6886ca2e267b726b32c061aa2c52c217044c6097
MonRoberto <roberto.quiroz117@outlook.com>
2024-10-01 17:54:37 -0600

    Numeros y Operadores

---
+++ js/05.js
@@ -0,0 +1,28 @@
+// Numeros
+
+//const nombre = "Mon";
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+
+const numero6 = 7;
+const numero7 = 6;
+
+//console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero6 % numero7);// Reciduo
+
+const cliente = 'Mon';
+const cliente2 = 'Roberto';
+
+console.log(cliente * cliente2); // NaN Not a Number
