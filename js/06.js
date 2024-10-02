8719e22cd96ecbe410edc62a251f45938577ce6b
MonRoberto <roberto.quiroz117@outlook.com>
2024-10-01 18:09:09 -0600

    El Objeto Math en JS

---
+++ js/06.js
@@ -0,0 +1,26 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado1 = Math.round(2.2); // Redondea
+resultado2 = Math.ceil(2.1); // Ceil siempre redondea hacia arriba
+resultado3 = Math.floor(2.1); // Floor siempre redondea hacia abajo
+resultado4 = Math.sqrt(144); // Raiz cuadrada
+resultado5 = Math.abs(-200); // abs convierte en positivo un numero negativo
+resultado6 = Math.min( 3, 5, 1, 8, 2, 10); // min da el minimo de un listado
+resultado7 = Math.max( 3, 5, 1, 8, 2, 10); // max da el maximo de un listado
+resultado8 = Math.random(); // random genera un numero aleatorio
+resultado9 = Math.floor(Math.random() * 30); // random genera un numero aleatorio
+
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+console.log(resultado6);
+console.log(resultado7);
+console.log(resultado8);
+console.log(resultado9);
