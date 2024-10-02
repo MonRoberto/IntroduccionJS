b6e6fde1ff05e7e7a6b5067405b7c1f746a8d42f
MonRoberto <roberto.quiroz117@outlook.com>
2024-10-01 18:39:30 -0600

    Booleans

---
+++ js/09.js
@@ -0,0 +1,14 @@
+// Booleans
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = 'true'; // String
+const boolean4 = true;
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
+console.log(typeof boolean4); // Tipo de dato que tiene un valor
+
+const boolean5 = new Boolean(true); // Constructor
+console.log(typeof boolean5); // Objeto
