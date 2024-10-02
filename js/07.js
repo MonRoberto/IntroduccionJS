dca8d2f0e79f97a93c3b989831c05e308c4396e4
MonRoberto <roberto.quiroz117@outlook.com>
2024-10-01 18:21:54 -0600

    Controlar el Orden de las Operaciones

---
+++ js/07.js
@@ -0,0 +1,26 @@
+// Orden de las Operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+resultado1 = (100 + 200 + 300) * .2;
+resultado2 = (600 + 600) * 1.16;
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+
+// Incrementos
+
+let puntaje = 10;
+
+// Incremento en 1 ++
+puntaje++;// 10
+++puntaje;// 12
+
+// Decremento en 1 --
+puntaje--;// 10
+--puntaje;// 12
+
+puntaje += 10;
+console.log(puntaje);
