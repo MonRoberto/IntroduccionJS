a91910c12c68a99ef7859647a45263fef98926b5
MonRoberto <monskate@outlook.com>
2024-10-12 20:14:11 +0200

    Funciones en JavaScript

---
+++ js/16.js
@@ -0,0 +1,20 @@
+// Declaración de Función
+
+function sumar() {
+    console.log(10 + 10);
+}
+
+sumar()
+
+// Expresión de la Función
+
+const sumar2 = function() {
+    console.log( 3 + 3);
+}
+
+sumar2();
+
+// IIFE
+(function() {
+    console.log('Esto es una función')
+})();
